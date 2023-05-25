72b987bca549b89604530ce3a9a0fef2708abbd5
CaioGunz <caiobarbosa2404@hotmail.com>
2023-05-25 16:51:39 -0300

    Montando os times

--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -7,16 +7,6 @@ import './Formulario.css'
 
 const Formulario = (props) => {
 
-    const times = [
-        'Programação',
-        'Front-End',
-        'Data-Science',
-        'Devops',
-        'UX e Desgin',
-        'Mobile',
-        'inovação e Gestão',
-    ]
-
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
     const [imagem, setImagem] = useState('')
@@ -30,6 +20,10 @@ const Formulario = (props) => {
             imagem,
             time
         })
+        setNome('')
+        setCargo('')
+        setImagem('')
+        setTimes('')
     }
 
     return (
@@ -59,7 +53,7 @@ const Formulario = (props) => {
                 <ListaSuspensa 
                     obrigatorio={true} 
                     label="Times" 
-                    itens={times}
+                    itens={props.times}
                     valor={time}
                     aoAlterado={valor => setTimes(valor)}
                 />
